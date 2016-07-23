48a885b2eb75446cb4234275f7caec753bf6da2b
Alex Barney <thealexbarney@gmail.com>
2016-07-22 23:44:56 -0500

    Add wrapper for dual-file .dsp streams

---
+++ DspAdpcm/DspAdpcm.Encode/Adpcm/Formats/DspLR.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace DspAdpcm.Encode.Adpcm.Formats
+{
+    public class DspLR
+    {
+        public Dsp LeftChannel { get; }
+        public Dsp RightChannel { get; }
+
+        public DspLR(IAdpcmStream stream)
+        {
+            if (stream.Channels.Count != 2)
+            {
+                throw new InvalidDataException($"Stream must have 2 channels, not {stream.Channels.Count}");
+            }
+
+            LeftChannel = new Dsp(stream.ShallowCloneChannelSubset(0, 0));
+            RightChannel = new Dsp(stream.ShallowCloneChannelSubset(1, 1));
+        }
+    }
+}
