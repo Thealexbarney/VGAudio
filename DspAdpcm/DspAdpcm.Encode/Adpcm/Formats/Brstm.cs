4ce53dce7de6518a606bad88c5eb8bd99e73851c
Alex Barney <thealexbarney@gmail.com>
2016-07-28 14:24:48 -0500

    Add parsing for brstm header

--- DspAdpcm/DspAdpcm.Encode/Adpcm/Formats/Brstm.cs
+++ DspAdpcm/DspAdpcm.Encode/Adpcm/Formats/Brstm.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using static DspAdpcm.Encode.Helpers;
 
@@ -9,23 +10,23 @@ namespace DspAdpcm.Encode.Adpcm.Formats
     public class Brstm
     {
         public IAdpcmStream AudioStream { get; set; }
-        
+
         private int NumSamples => AudioStream.NumSamples;
         private int NumChannels => AudioStream.Channels.Count;
         private byte Codec { get; } = 2; // 4-bit ADPCM
         private byte Looping => (byte)(AudioStream.Looping ? 1 : 0);
         private int AudioDataOffset => DataChunkOffset + 0x20;
         private int InterleaveSize => GetBytesForAdpcmSamples(SamplesPerInterleave);
-        private int SamplesPerInterleave => 0x3800;
+        private int SamplesPerInterleave { get; set; } = 0x3800;
         private int InterleaveCount => (NumSamples / SamplesPerInterleave) + (LastBlockSamples == 0 ? 0 : 1);
         private int LastBlockSizeWithoutPadding => GetBytesForAdpcmSamples(LastBlockSamples);
         private int LastBlockSamples => NumSamples % SamplesPerInterleave;
         private int LastBlockSize => GetNextMultiple(LastBlockSizeWithoutPadding, 0x20);
-        private int SamplesPerAdpcEntry => 0x3800;
+        private int SamplesPerAdpcEntry { get; set; } = 0x3800;
         private int NumAdpcEntries => (NumSamples / SamplesPerAdpcEntry) - (LastBlockSamples == 0 ? 1 : 0);
         private int BytesPerAdpcEntry => 4; //Or is it bits per sample?
 
-        private int RstmHeaderLength => 0x40;
+        private int RstmHeaderLength { get; set; } = 0x40;
 
         private int HeadChunkOffset => RstmHeaderLength;
         private int HeadChunkLength => GetNextMultiple(HeadChunkHeaderLength + HeadChunkTableLength +
@@ -33,7 +34,7 @@ namespace DspAdpcm.Encode.Adpcm.Formats
         private int HeadChunkHeaderLength = 8;
         private int HeadChunkTableLength => 8 * 3;
         private int HeadChunk1Length => 0x34;
-        private int HeadChunk2Length => 0x10;
+        private int HeadChunk2Length { get; set; } = 0x10;
         private int HeadChunk3Length => 4 + (8 * NumChannels) + (ChannelInfoLength * NumChannels);
         private int ChannelInfoLength => 0x38;
 
@@ -53,6 +54,11 @@ namespace DspAdpcm.Encode.Adpcm.Formats
             }
 
             AudioStream = stream;
+        }
+
+        public Brstm(Stream stream)
+        {
+            ReadBrstmFile(stream);
         }
 
         public IEnumerable<byte> GetFile()
@@ -219,6 +225,190 @@ namespace DspAdpcm.Encode.Adpcm.Formats
             chunk.AddRange(channels.Interleave(InterleaveSize, LastBlockSize));
 
             return chunk.ToArray();
+        }
+
+        public void ReadBrstmFile(Stream stream)
+        {
+            using (var reader = new BinaryReader(stream))
+            {
+                if (Encoding.UTF8.GetString(reader.ReadBytes(4), 0, 4) != "RSTM")
+                {
+                    throw new InvalidDataException("File has no RSTM header");
+                }
+
+                var structure = new BrstmStructure();
+
+                reader.BaseStream.Position = 8;
+                structure.FileLength = reader.ReadInt32BE();
+                structure.RstmHeaderLength = reader.ReadInt16BE();
+                reader.BaseStream.Position += 2;
+
+                structure.HeadChunkOffset = reader.ReadInt32BE();
+                structure.HeadChunkLengthRstm = reader.ReadInt32BE();
+                structure.AdpcChunkOffset = reader.ReadInt32BE();
+                structure.AdpcChunkLengthRstm = reader.ReadInt32BE();
+                structure.DataChunkOffset = reader.ReadInt32BE();
+                structure.DataChunkLengthRstm = reader.ReadInt32BE();
+
+                reader.BaseStream.Position = structure.HeadChunkOffset;
+                byte[] headChunk = reader.ReadBytes(structure.HeadChunkLengthRstm);
+                ParseHeadChunk(headChunk, structure);
+            }
+        }
+
+        private static void ParseHeadChunk(byte[] head, BrstmStructure structure)
+        {
+            int baseOffset = 8;
+
+            if (Encoding.UTF8.GetString(head, 0, 4) != "HEAD")
+            {
+                throw new InvalidDataException("Unknown or invalid HEAD chunk");
+            }
+
+            using (var reader = new BinaryReader(new MemoryStream(head)))
+            {
+                reader.BaseStream.Position = 4;
+                if (reader.ReadInt32BE() != head.Length)
+                {
+                    throw new InvalidDataException("HEAD chunk stated length does not match actual length");
+                }
+
+                reader.BaseStream.Position += 4;
+                structure.HeadChunk1Offset = reader.ReadInt32BE();
+                reader.BaseStream.Position += 4;
+                structure.HeadChunk2Offset = reader.ReadInt32BE();
+                reader.BaseStream.Position += 4;
+                structure.HeadChunk3Offset = reader.ReadInt32BE();
+
+                reader.BaseStream.Position = structure.HeadChunk1Offset + baseOffset;
+                byte[] headChunk1 = reader.ReadBytes(structure.HeadChunk1Length);
+
+                reader.BaseStream.Position = structure.HeadChunk3Offset + baseOffset;
+                byte[] headChunk3 = reader.ReadBytes(structure.HeadChunk3Length);
+
+                ParseHeadChunk1(headChunk1, structure);
+                ParseHeadChunk3(headChunk3, structure);
+            }
+        }
+
+        private static void ParseHeadChunk1(byte[] chunk, BrstmStructure structure)
+        {
+            using (var reader = new BinaryReader(new MemoryStream(chunk)))
+            {
+                structure.Codec = reader.ReadByte();
+                if (structure.Codec != 2) //4-bit ADPCM codec
+                {
+                    throw new InvalidDataException("File must contain 4-bit ADPCM encoded audio");
+                }
+
+                structure.Looping = reader.ReadByte() == 1;
+                structure.NumChannelsChunk1 = reader.ReadByte();
+                reader.BaseStream.Position += 1;
+
+                structure.SampleRate = reader.ReadInt16BE();
+                reader.BaseStream.Position += 2;
+
+                structure.LoopStart = reader.ReadInt32BE();
+                structure.NumSamples = reader.ReadInt32BE();
+
+                structure.AudioDataOffset = reader.ReadInt32BE();
+                structure.InterleaveCount = reader.ReadInt32BE();
+                structure.InterleaveSize = reader.ReadInt32BE();
+                structure.SamplesPerInterleave = reader.ReadInt32BE();
+                structure.LastBlockSizeWithoutPadding = reader.ReadInt32BE();
+                structure.LastBlockSamples = reader.ReadInt32BE();
+                structure.LastBlockSize = reader.ReadInt32BE();
+                structure.SamplesPerAdpcEntry = reader.ReadInt32BE();
+            }
+        }
+
+        private static void ParseHeadChunk3(byte[] chunk, BrstmStructure structure)
+        {
+            using (var reader = new BinaryReader(new MemoryStream(chunk)))
+            {
+                structure.NumChannelsChunk3 = reader.ReadByte();
+                reader.BaseStream.Position += 3;
+
+                var channels = new List<ChannelInfo>();
+
+                for (int i = 0; i < structure.NumChannelsChunk3; i++)
+                {
+                    var channel = new ChannelInfo();
+                    reader.BaseStream.Position += 4;
+                    channel.Offset = reader.ReadInt32BE();
+                    channels.Add(channel);
+                }
+
+                int baseOffset = structure.HeadChunk3Offset;
+                foreach (ChannelInfo channel in channels)
+                {
+                    reader.BaseStream.Position = channel.Offset - structure.HeadChunk3Offset + 4;
+                    int coefsOffset = reader.ReadInt32BE();
+                    reader.BaseStream.Position = coefsOffset - structure.HeadChunk3Offset;
+
+                    channel.Coefs = Enumerable.Range(0, 16).Select(x => reader.ReadInt16BE()).ToArray();
+                    channel.Gain = reader.ReadInt16BE();
+                    channel.PredScale = reader.ReadInt16BE();
+                    channel.Hist1 = reader.ReadInt16BE();
+                    channel.Hist2 = reader.ReadInt16BE();
+                    channel.LoopPredScale = reader.ReadInt16BE();
+                    channel.LoopHist1 = reader.ReadInt16BE();
+                    channel.LoopHist2 = reader.ReadInt16BE();
+                }
+            }
+        }
+
+        private class BrstmStructure
+        {
+            public int FileLength { get; set; }
+            public int RstmHeaderLength { get; set; }
+            public int HeadChunkOffset { get; set; }
+            public int HeadChunkLengthRstm { get; set; }
+            public int AdpcChunkOffset { get; set; }
+            public int AdpcChunkLengthRstm { get; set; }
+            public int DataChunkOffset { get; set; }
+            public int DataChunkLengthRstm { get; set; }
+
+            public int HeadChunkLength { get; set; }
+            public int HeadChunk1Offset { get; set; }
+            public int HeadChunk2Offset { get; set; }
+            public int HeadChunk3Offset { get; set; }
+            public int HeadChunk1Length => HeadChunk2Offset - HeadChunk1Offset;
+            public int HeadChunk2Length => HeadChunk3Offset - HeadChunk2Offset;
+            public int HeadChunk3Length => AdpcChunkOffset - HeadChunk3Offset;
+
+            public int Codec { get; set; }
+            public bool Looping { get; set; }
+            public int NumChannelsChunk1 { get; set; }
+            public int SampleRate { get; set; }
+            public int LoopStart { get; set; }
+            public int NumSamples { get; set; }
+            public int AudioDataOffset { get; set; }
+            public int InterleaveCount { get; set; }
+            public int InterleaveSize { get; set; }
+            public int SamplesPerInterleave { get; set; }
+            public int LastBlockSizeWithoutPadding { get; set; }
+            public int LastBlockSamples { get; set; }
+            public int LastBlockSize { get; set; }
+            public int SamplesPerAdpcEntry { get; set; }
+
+            public int NumChannelsChunk3 { get; set; }
+        }
+
+        private class ChannelInfo
+        {
+            public int Offset { get; set; }
+
+            public short[] Coefs { get; set; }
+
+            public short Gain { get; set; }
+            public short PredScale { get; set; }
+            public short Hist1 { get; set; } = 0;
+            public short Hist2 { get; set; } = 0;
+
+            public short LoopPredScale { get; set; }
+            public short LoopHist1 { get; set; }
+            public short LoopHist2 { get; set; }
         }
     }
 }
