1cf8847a5a23dce6d9accdce6a8fa31c49bb2a66
Alex Barney <thealexbarney@gmail.com>
2016-09-01 18:42:21 -0500

    IDSP reading support

--- DspAdpcm/DspAdpcm/Adpcm/Formats/Idsp.cs
+++ DspAdpcm/DspAdpcm/Adpcm/Formats/Idsp.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
+using static DspAdpcm.Helpers;
 
 namespace DspAdpcm.Adpcm.Formats
 {
@@ -22,7 +24,7 @@ namespace DspAdpcm.Adpcm.Formats
         /// <summary>
         /// The size in bytes of the IDSP file.
         /// </summary>
-        public int FileLength => 0;// RstmHeaderLength + HeadChunkLength + AdpcChunkLength + DataChunkLength;
+        public int FileLength => 0; // Placeholder
 
         /// <summary>
         /// Initializes a new <see cref="Idsp"/> from an <see cref="AdpcmStream"/>.
@@ -172,13 +174,98 @@ namespace DspAdpcm.Adpcm.Formats
 
                 var structure = new IdspStructure();
 
+                ParseIdspHeader(reader, structure);
+                if (readAudioData)
+                {
+                    ParseIdspData(reader, structure);
+                    SetProperties(structure);
+                }
+
                 return structure;
             }
         }
-    }
 
-    public class IdspConfiguration
-    {
-        
+        private void SetProperties(IdspStructure structure)
+        {
+            //Todo: set configuration
+
+            AudioStream = new AdpcmStream(structure.NumSamples, structure.SampleRate);
+            if (structure.Looping)
+            {
+                AudioStream.SetLoop(structure.LoopStart, structure.NumSamples);
+            }
+
+            for (int c = 0; c < structure.NumChannels; c++)
+            {
+                var channel = new AdpcmChannel(structure.NumSamples, structure.AudioData[c])
+                {
+                    Coefs = structure.Channels[c].Coefs,
+                    Gain = structure.Channels[c].Gain,
+                    Hist1 = structure.Channels[c].Hist1,
+                    Hist2 = structure.Channels[c].Hist2,
+                };
+                channel.SetLoopContext(structure.Channels[c].LoopPredScale, structure.Channels[c].LoopHist1,
+                    structure.Channels[c].LoopHist2);
+                AudioStream.Channels.Add(channel);
+            }
+        }
+
+        private static void ParseIdspHeader(BinaryReader reader, IdspStructure structure)
+        {
+            reader.BaseStream.Position += 4;
+            structure.NumChannels = reader.ReadInt32();
+            structure.SampleRate = reader.ReadInt32();
+            structure.NumSamples = reader.ReadInt32();
+            structure.LoopStart = reader.ReadInt32();
+            structure.LoopEnd = reader.ReadInt32();
+            structure.InterleaveSize = reader.ReadInt32();
+            structure.HeaderSize = reader.ReadInt32();
+            structure.ChannelInfoSize = reader.ReadInt32();
+            structure.AudioDataOffset = reader.ReadInt32();
+            structure.ChannelSize = reader.ReadInt32();
+
+            for (int i = 0; i < structure.NumChannels; i++)
+            {
+                reader.BaseStream.Position = structure.HeaderSize + i * structure.ChannelInfoSize;
+                var channel = new IDSPChannelInfo();
+                channel.NumSamples = reader.ReadInt32();
+                channel.NumNibbles = reader.ReadInt32();
+                channel.SampleRate = reader.ReadInt32();
+                channel.Looping = reader.ReadInt16() == 1;
+                reader.BaseStream.Position += 2;
+                channel.StartLoop = reader.ReadInt32();
+                channel.EndLoop = reader.ReadInt32();
+                channel.Unknown = reader.ReadInt32();
+                channel.Coefs = Enumerable.Range(0, 16).Select(x => reader.ReadInt16()).ToArray();
+                channel.Gain = reader.ReadInt16();
+                channel.PredScale = reader.ReadInt16();
+                channel.Hist1 = reader.ReadInt16();
+                channel.Hist2 = reader.ReadInt16();
+                channel.LoopPredScale = reader.ReadInt16();
+                channel.LoopHist1 = reader.ReadInt16();
+                channel.LoopHist2 = reader.ReadInt16();
+
+                structure.Channels.Add(channel);
+            }
+
+            //There isn't a loop flag for the file in general, so we set it
+            //if any of the single channels loop.
+            structure.Looping = structure.Channels.Any(x => x.Looping);
+        }
+
+        private static void ParseIdspData(BinaryReader reader, IdspStructure structure)
+        {
+            reader.BaseStream.Position = structure.AudioDataOffset;
+            int interleave = structure.InterleaveSize == 0 ? structure.ChannelSize : structure.InterleaveSize;
+            //If the file isn't interleaved, there is no padding/alignment at the break between channels.
+            int length = GetNextMultiple(structure.ChannelSize, interleave) * structure.NumChannels;
+            structure.AudioData = reader.BaseStream.DeInterleave(length, interleave,
+                structure.NumChannels);
+        }
+
+        public class IdspConfiguration
+        {
+
+        }
     }
 }
--- DspAdpcm/DspAdpcm/Adpcm/Formats/IdspStructure.cs
+++ DspAdpcm/DspAdpcm/Adpcm/Formats/IdspStructure.cs
@@ -1,6 +1,100 @@
-﻿namespace DspAdpcm.Adpcm.Formats
+﻿using System.Collections.Generic;
+
+namespace DspAdpcm.Adpcm.Formats
 {
+    /// <summary>
+    /// Defines the partial common structure and metadata
+    /// of IDSP files.
+    /// </summary>
     public class IdspStructure
     {
+        /// <summary>
+        /// This flag is set if the file loops.
+        /// </summary>
+        public bool Looping { get; set; }
+        /// <summary>
+        /// The number of channels in the file.
+        /// </summary>
+        public int NumChannels { get; set; }
+        /// <summary>
+        /// The sample rate of the audio.
+        /// </summary>
+        public int SampleRate { get; set; }
+        /// <summary>
+        /// The start loop point in samples.
+        /// </summary>
+        public int LoopStart { get; set; }
+        /// <summary>
+        /// The end loop point in samples.
+        /// </summary>
+        public int LoopEnd { get; set; }
+        /// <summary>
+        /// The number of samples in the file.
+        /// </summary>
+        public int NumSamples { get; set; }
+        /// <summary>
+        /// The offset that the actual audio data starts at.
+        /// </summary>
+        public int AudioDataOffset { get; set; }
+        /// <summary>
+        /// The number of bytes per channel in each
+        /// interleaved audio data block.
+        /// </summary>
+        public int InterleaveSize { get; set; }
+        /// <summary>
+        /// The size, in bytes, of the main IDSP header.
+        /// </summary>
+        public int HeaderSize { get; set; }
+        /// <summary>
+        /// The size, in bytes, of the information for each channel
+        /// in the file header.
+        /// </summary>
+        public int ChannelInfoSize { get; set; }
+        /// <summary>
+        /// The length, in bytes, of the audio data of each channel.
+        /// </summary>
+        public int ChannelSize { get; set; }
+        /// <summary>
+        /// The ADPCM information for each channel.
+        /// </summary>
+        public List<IDSPChannelInfo> Channels { get; set; } = new List<IDSPChannelInfo>();
+        internal byte[][] AudioData { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the ADPCM information for a single
+    /// ADPCM channel.
+    /// Used in IDSP files.
+    /// </summary>
+    public class IDSPChannelInfo : AdpcmChannelInfo
+    {
+        /// <summary>
+        /// The number of samples in the channel.
+        /// </summary>
+        public int NumSamples { get; set; }
+        /// <summary>
+        /// The number of ADPCM nibbles in the channel.
+        /// </summary>
+        public int NumNibbles { get; set; }
+        /// <summary>
+        /// The sample rate of the audio.
+        /// </summary>
+        public int SampleRate { get; set; }
+        /// <summary>
+        /// This flag is set if the channel loops.
+        /// </summary>
+        public bool Looping { get; set; }
+        /// <summary>
+        /// The end loop point in nibbles.
+        /// </summary>
+        public int StartLoop { get; set; }
+        /// <summary>
+        /// The end loop point in nibbles.
+        /// </summary>
+        public int EndLoop { get; set; }
+        /// <summary>
+        /// Not sure what this value is used for.
+        /// </summary>
+        public int Unknown { get; set; }
     }
 }
