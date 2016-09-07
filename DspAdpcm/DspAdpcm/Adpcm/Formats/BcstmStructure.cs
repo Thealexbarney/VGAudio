a90b5355970a3c5354b5091dd09ffebac218dfd0
Alex Barney <thealexbarney@gmail.com>
2016-09-07 00:21:40 -0500

    Add some missing documentation

--- DspAdpcm/DspAdpcm/Adpcm/Formats/BcstmStructure.cs
+++ DspAdpcm/DspAdpcm/Adpcm/Formats/BcstmStructure.cs
@@ -1,5 +1,9 @@
 ﻿namespace DspAdpcm.Adpcm.Formats
 {
+    /// <summary>
+    /// Defines the structure and metadata
+    /// of a BCSTM file.
+    /// </summary>
     public class BcstmStructure : BCFstmStructure
     {
     }
--- DspAdpcm/DspAdpcm/Adpcm/Formats/BfstmStructure.cs
+++ DspAdpcm/DspAdpcm/Adpcm/Formats/BfstmStructure.cs
@@ -1,5 +1,9 @@
 ﻿namespace DspAdpcm.Adpcm.Formats
 {
+    /// <summary>
+    /// Defines the structure and metadata
+    /// of a BFSTM file.
+    /// </summary>
     public class BfstmStructure : BCFstmStructure
     {
     }
