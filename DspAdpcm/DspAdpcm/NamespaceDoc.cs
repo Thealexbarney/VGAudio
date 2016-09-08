f9e19889fe8d7d6c206f6f4be250579d2409e932
Alex Barney <thealexbarney@gmail.com>
2016-09-07 20:03:01 -0500

    Add namespace XML documentation

---
+++ DspAdpcm/DspAdpcm/NamespaceDoc.cs
@@ -0,0 +1,72 @@
+﻿// ReSharper disable CheckNamespace
+
+namespace DspAdpcm.Adpcm
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Adpcm"/> namespace contains classes
+    /// for representing, encoding, decoding, and manipulating audio data in 
+    /// Nintendo's DSP-ADPCM audio format.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
+
+namespace DspAdpcm.Adpcm.Formats
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Adpcm.Formats"/> namespace contains classes
+    /// for reading and writing various DSP-ADPCM audio containers.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
+
+namespace DspAdpcm.Adpcm.Formats.Configuration
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Adpcm.Formats.Configuration"/> namespace contains configuration
+    /// classes and options for the various DSP-ADPCM audio containers.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
+
+namespace DspAdpcm.Adpcm.Formats.Internal
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Adpcm.Formats.Internal"/> namespace contains internal and abstract
+    /// base classes that are related to the DSP-ADPCM formats. These classes should not be accessed directly.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
+
+namespace DspAdpcm.Adpcm.Formats.Structures
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Adpcm.Formats.Structures"/> namespace contains data classes
+    /// that are used for storing information about the various DSP-ADPCM audio containers.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
+
+namespace DspAdpcm.Pcm
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Pcm"/> namespace contains classes
+    /// for representing and manipulating 16-bit PCM audio data.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
+
+namespace DspAdpcm.Pcm.Formats
+{
+    /// <summary>
+    /// The <see cref="DspAdpcm.Pcm.Formats"/> namespace contains classes
+    /// for reading and writing PCM audio containers.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
