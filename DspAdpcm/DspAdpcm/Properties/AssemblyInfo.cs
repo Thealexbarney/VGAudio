951ea89ddbe0d9ff04ca4a8dd09c40a838156b2d
Alex Barney <thealexbarney@gmail.com>
2016-09-07 20:15:59 -0500

    Make MSBuild happy by adding attributes back to AssemblyInfo.cs.

--- DspAdpcm/DspAdpcm/Properties/AssemblyInfo.cs
+++ DspAdpcm/DspAdpcm/Properties/AssemblyInfo.cs
@@ -1,6 +1,11 @@
 ﻿using System.Reflection;
 using System.Resources;
 
+[assembly: AssemblyTitle("DspAdpcm")]
+[assembly: AssemblyDescription("A library for encoding, decoding, and manipulating audio files using Nintendo's DSP-ADPCM format.")]
+[assembly: AssemblyCopyright("Copyright © 2016 Alex Barney")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyProduct("DspAdpcm")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
