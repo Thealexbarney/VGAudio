d2072c6fb09c0d459cb55d9e96a4876abbfb3b09
Alex Barney <thealexbarney@gmail.com>
2016-09-17 22:49:09 -0500

    Tag 1.1.0

--- DspAdpcm/DspAdpcm/Properties/AssemblyInfo.cs
+++ DspAdpcm/DspAdpcm/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Resources;
 [assembly: AssemblyTitle("DspAdpcm")]
 [assembly: AssemblyDescription("A library for encoding, decoding, and manipulating audio files using Nintendo's DSP-ADPCM format.")]
 [assembly: AssemblyCopyright("Copyright © 2016 Alex Barney")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyProduct("DspAdpcm")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
