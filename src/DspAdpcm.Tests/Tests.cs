61198718fa41166df647f9fd7a9329ca63a0f844
Alex Barney <thealexbarney@gmail.com>
2016-09-29 23:22:07 -0500

    Add tests project

---
+++ src/DspAdpcm.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace DspAdpcm.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            DspAdpcm.Helpers.GetNibbleFromSample(4);
+            Assert.Equal(4, 4);
+        }
+    }
+}
