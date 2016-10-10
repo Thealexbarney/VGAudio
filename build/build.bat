88cb96fe05d363bf8f2a3301a12d32b242f98cb5
Alex Barney <thealexbarney@gmail.com>
2016-10-10 14:39:18 -0500

    Move build scripts to their own directory

---
+++ build/build.bat
@@ -0,0 +1,24 @@
+echo off
+
+set sourceDir=..\src
+set outDir=..\bin
+
+rmdir /s /q %outDir% 2>NUL
+rmdir /s /q %sourceDir%\DspAdpcm\bin 2>NUL
+rmdir /s /q %sourceDir%\DspAdpcm\obj 2>NUL
+rmdir /s /q %sourceDir%\DspAdpcm.Cli\bin 2>NUL
+rmdir /s /q %sourceDir%\DspAdpcm.Cli\obj 2>NUL
+rmdir /s /q %sourceDir%\DspAdpcm.Tests\bin 2>NUL
+rmdir /s /q %sourceDir%\DspAdpcm.Tests\obj 2>NUL
+
+del /s %sourceDir%\*.lock.json 2>NUL
+del /s %sourceDir%\*.nuget.targets 2>NUL
+
+dotnet restore %sourceDir%
+
+dotnet pack %sourceDir%\DspAdpcm -c release -o %outDir%\NuGet
+
+dotnet publish %sourceDir%\DspAdpcm.Cli -c release -f net45 -o %outDir%\Cli\net45
+dotnet publish %sourceDir%\DspAdpcm.Cli -c release -f netcoreapp1.0 -o %outDir%\Cli\netcoreapp1.0
+
+dotnet test %sourceDir%\DspAdpcm.Tests -c release
