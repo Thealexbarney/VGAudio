71809ff440f44142d782e56e1571f0b76824543e
Alex Barney <thealexbarney@gmail.com>
2016-10-21 21:36:46 -0500

    Add and combine more build/parse benchmarks. Add fast config for benchmarks

--- src/DspAdpcm.Benchmark/Runner.cs
+++ src/DspAdpcm.Benchmark/Runner.cs
@@ -1,22 +1,53 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnostics.Windows;
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Validators;
 using DspAdpcm.Benchmark.AdpcmBenchmarks;
+using DspAdpcm.Benchmark.BuildParse;
+using DspAdpcm.Benchmark.HelperBenchmarks;
 
 namespace DspAdpcm.Benchmark
 {
     public static class Runner
     {
+        private static Type[] Benchmarks { get; } =
+        {
+            typeof(BuildParseBenchmarks),
+            typeof(CoefBenchmarks),
+            typeof(EncodeBenchmarks),
+            typeof(InterleaveBenchmarks)
+        };
+
+        public static IConfig FastConfig { get; } =
+            ManualConfig
+            .Create(DefaultConfig.Instance)
+            .With(Job.RyuJitX64
+                .WithLaunchCount(1)
+                .WithIterationTime(100)
+                .WithWarmupCount(3)
+                .WithTargetCount(3)
+            );
+
         public static void Main(string[] args)
         {
-            BenchmarkRunner.Run<CoefBenchmarks>(
-                ManualConfig
-                    .Create(DefaultConfig.Instance)
-                    .With(Job.RyuJitX64)
-                    .With(new MemoryDiagnoser())
-                    .With(ExecutionValidator.FailOnError));
+            RunFull(Benchmarks);
+        }
+
+        private static void RunFull(IEnumerable<Type> benchmarks)
+        {
+            foreach (Type benchmark in benchmarks)
+            {
+                BenchmarkRunner.Run(benchmark);
+            }
+        }
+
+        private static void RunFast(IEnumerable<Type> benchmarks)
+        {
+            foreach (Type benchmark in benchmarks)
+            {
+                BenchmarkRunner.Run(benchmark, FastConfig);
+            }
         }
     }
 }
