50561f74745835f676488f06190b5cc5891c56d5
Alex Barney <thealexbarney@gmail.com>
2016-10-22 21:39:24 -0500

    CLI: Add class to convert between file types

---
+++ src/DspAdpcm.Cli/Convert.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DspAdpcm.Adpcm;
+using DspAdpcm.Adpcm.Formats;
+using DspAdpcm.Adpcm.Formats.Configuration;
+using DspAdpcm.Pcm;
+using DspAdpcm.Pcm.Formats;
+
+namespace DspAdpcm.Cli
+{
+    internal class Convert
+    {
+        private Convert() {}
+        private PcmStream Pcm { get; set; }
+        private AdpcmStream Adpcm { get; set; }
+        private object Configuration { get; set; }
+
+        public static bool ConvertFile(Options options)
+        {
+            if (options.Job != JobType.Convert) return false;
+
+            var converter = new Convert();
+            converter.ReadFile(options.InFilePath, options.InFileType);
+
+            if (!(options.InFileType == options.OutFileType && options.KeepConfiguration))
+            {
+                converter.Configuration = null;
+            }
+
+            converter.EncodeFile(options);
+            converter.WriteFile(options.OutFilePath, options.OutFileType);
+
+            return true;
+        }
+
+        private void ReadFile(string fileName, FileType fileType)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                switch (fileType)
+                {
+                    case FileType.Wave:
+                        Pcm = new Wave(stream).AudioStream;
+                        break;
+                    case FileType.Dsp:
+                        var dsp = new Dsp(stream);
+                        Adpcm = dsp.AudioStream;
+                        Configuration = dsp.Configuration;
+                        break;
+                    case FileType.Idsp:
+                        var idsp = new Idsp(stream);
+                        Adpcm = idsp.AudioStream;
+                        Configuration = idsp.Configuration;
+                        break;
+                    case FileType.Brstm:
+                        var brstm = new Brstm(stream);
+                        Adpcm = brstm.AudioStream;
+                        Configuration = brstm.Configuration;
+                        break;
+                    case FileType.Bcstm:
+                        var bcstm = new Bcstm(stream);
+                        Adpcm = bcstm.AudioStream;
+                        Configuration = bcstm.Configuration;
+                        break;
+                    case FileType.Bfstm:
+                        var bfstm = new Bfstm(stream);
+                        Adpcm = bfstm.AudioStream;
+                        Configuration = bfstm.Configuration;
+                        break;
+                    case FileType.Genh:
+                        Adpcm = new Genh(stream).AudioStream;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(fileType), fileType, null);
+                }
+            }
+        }
+
+        private void WriteFile(string fileName, FileType fileType)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                switch (fileType)
+                {
+                    case FileType.Wave:
+                        new Wave(Pcm).WriteFile(stream);
+                        break;
+                    case FileType.Dsp:
+                        new Dsp(Adpcm, (DspConfiguration)Configuration).WriteFile(stream);
+                        break;
+                    case FileType.Idsp:
+                        new Idsp(Adpcm, (IdspConfiguration)Configuration).WriteFile(stream);
+                        break;
+                    case FileType.Brstm:
+                        new Brstm(Adpcm, (BrstmConfiguration)Configuration).WriteFile(stream);
+                        break;
+                    case FileType.Bcstm:
+                        new Bcstm(Adpcm, (BcstmConfiguration)Configuration).WriteFile(stream);
+                        break;
+                    case FileType.Bfstm:
+                        new Bfstm(Adpcm, (BfstmConfiguration)Configuration).WriteFile(stream);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(fileType), fileType, null);
+                }
+            }
+        }
+
+        private void EncodeFile(Options options)
+        {
+            AudioType inType = AudioTypes[options.InFileType];
+            AudioType outType = AudioTypes[options.OutFileType];
+            if (inType == outType) return;
+
+            if (inType == AudioType.Pcm && outType == AudioType.Adpcm)
+            {
+                Adpcm = Encode.PcmToAdpcm(Pcm);
+            }
+
+            if (inType == AudioType.Adpcm && outType == AudioType.Pcm)
+            {
+                Pcm = Decode.AdpcmtoPcm(Adpcm);
+            }
+        }
+
+        private static readonly Dictionary<FileType, AudioType> AudioTypes = 
+            new Dictionary<FileType, AudioType>
+            {
+                [FileType.Wave] = AudioType.Pcm,
+                [FileType.Dsp] = AudioType.Adpcm,
+                [FileType.Idsp] = AudioType.Adpcm,
+                [FileType.Brstm] = AudioType.Adpcm,
+                [FileType.Bcstm] = AudioType.Adpcm,
+                [FileType.Bfstm] = AudioType.Adpcm,
+                [FileType.Genh] = AudioType.Adpcm
+            };
+    }
+}
---
+++ src/DspAdpcm.Cli/Options.cs
@@ -0,0 +1,39 @@
+﻿namespace DspAdpcm.Cli
+{
+    internal class Options
+    {
+        public JobType Job { get; set; }
+
+        public FileType InFileType { get; set; }
+        public string InFilePath { get; set; }
+
+        public FileType OutFileType { get; set; }
+        public string OutFilePath { get; set; }
+
+        public bool KeepConfiguration { get; set; }
+    }
+
+    internal enum JobType
+    {
+        Convert,
+        Batch,
+        Metadata
+    }
+
+    internal enum FileType
+    {
+        Wave,
+        Dsp,
+        Idsp,
+        Brstm,
+        Bcstm,
+        Bfstm,
+        Genh
+    }
+
+    internal enum AudioType
+    {
+        Pcm,
+        Adpcm
+    }
+}
