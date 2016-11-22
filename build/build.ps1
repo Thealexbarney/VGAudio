99d798d01d28c24c3d225964f6d411118818d5e7
Alex Barney <thealexbarney@gmail.com>
2016-11-21 21:05:12 -0500

    Add bare psake build scripts. Take some guidance from Json.NET's build scripts

---
+++ build/build.ps1
@@ -0,0 +1,23 @@
+﻿properties { 
+
+}
+
+framework '4.6x86'
+
+task default -depends Test
+
+task Clean {
+
+}
+
+task Build -depends Clean {
+  
+}
+
+task Package -depends Build {
+  
+}
+
+task Test -depends Package {
+      
+}
---
+++ build/runbuild.cmd
@@ -0,0 +1,5 @@
+cls
+powershell -ExecutionPolicy Bypass -Command "& { Start-Transcript '%~dp0runbuild.txt'; Import-Module '%~dp0..\tools\psake.psm1'; Invoke-psake '%~dp0..\build\build.ps1' %*; Stop-Transcript; exit !($psake.build_success); }"
+
+ECHO %ERRORLEVEL%
+EXIT /B %ERRORLEVEL%
