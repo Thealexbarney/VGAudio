678ede74ddce64d07b046eee9522eec7d87ab702
Alex Barney <thealexbarney@gmail.com>
2017-02-25 22:32:14 -0600

    Move CLI runner out of main

--- src/DspAdpcm.Cli/DspAdpcmCli.cs
+++ src/DspAdpcm.Cli/DspAdpcmCli.cs
@@ -1,34 +1,10 @@
-﻿using System;
-using System.Diagnostics;
-
-namespace DspAdpcm.Cli
+﻿namespace DspAdpcm.Cli
 {
     public static class DspAdpcmCli
     {
         public static int Main(string[] args)
         {
-            Options options = CliArguments.Parse(args);
-
-            if (options == null) return 1;
-
-            if (options.Job == JobType.Convert)
-            {
-                Stopwatch watch = Stopwatch.StartNew();
-                bool success = Convert.ConvertFile(options);
-                watch.Stop();
-
-                if (success)
-                {
-                    Console.WriteLine("Success!");
-                    Console.WriteLine($"Time elapsed: {watch.Elapsed.TotalSeconds}");
-                }
-                else
-                {
-                    return 1;
-                }
-            }
-            
-            return 0;
+            return Converter.RunConverterCli(args) ? 0 : 1;
         }
     }
 }
