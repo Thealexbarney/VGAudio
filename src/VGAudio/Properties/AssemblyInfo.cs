0470feda5035b2cce2dfda03bffe236e131a5745
Alex Barney <thealexbarney@gmail.com>
2017-02-27 20:42:57 -0600

    Move library, cli and tests to msbuild

--- src/VGAudio/Properties/AssemblyInfo.cs
+++ src/VGAudio/Properties/AssemblyInfo.cs
@@ -2,12 +2,5 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("VGAudio")]
-[assembly: AssemblyDescription("A library for encoding, decoding, and manipulating audio files using Nintendo's DSP-ADPCM format.")]
-[assembly: AssemblyCopyright("Copyright © 2016 Alex Barney")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyProduct("VGAudio")]
-[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: InternalsVisibleTo("VGAudio.Tests")]
 [assembly: InternalsVisibleTo("VGAudio.Benchmark")]
