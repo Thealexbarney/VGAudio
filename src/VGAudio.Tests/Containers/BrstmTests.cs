6a46bd68d75cd642aea88e0ad4c50d7a1c6b38da
Alex Barney <thealexbarney@gmail.com>
2017-04-29 18:49:35 -0500

    Add simple pcm brstm tests

--- src/VGAudio.Tests/Containers/BrstmTests.cs
+++ src/VGAudio.Tests/Containers/BrstmTests.cs
@@ -1,4 +1,5 @@
 ﻿using VGAudio.Containers;
+using VGAudio.Containers.Bxstm;
 using VGAudio.Formats;
 using Xunit;
 
@@ -15,6 +16,18 @@ namespace VGAudio.Tests.Containers
             GcAdpcmFormat audio = GenerateAudio.GenerateAdpcmSineWave(BuildParseTestOptions.Samples, numChannels, BuildParseTestOptions.SampleRate);
 
             BuildParseTests.BuildParseCompareAudio(audio, new BrstmWriter(), new BrstmReader());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(8)]
+        public void BrstmBuildAndParseEqualPcm(int numChannels)
+        {
+            Pcm16Format audio = GenerateAudio.GeneratePcmSineWave(BuildParseTestOptions.Samples, numChannels, BuildParseTestOptions.SampleRate);
+            var writer = new BrstmWriter {Configuration = {Codec = BxstmCodec.Pcm16Bit}};
+
+            BuildParseTests.BuildParseCompareAudio(audio, writer, new BrstmReader());
         }
     }
 }
