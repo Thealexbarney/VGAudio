d87c6dbfede5defaead73a291fdacccb7de7cbc0
Alex Barney <thealexbarney@gmail.com>
2017-05-03 20:14:26 -0500

    Add simple PCM B[C|F]STM tests

--- src/VGAudio.Tests/Containers/BfstmTests.cs
+++ src/VGAudio.Tests/Containers/BfstmTests.cs
@@ -1,4 +1,5 @@
 ﻿using VGAudio.Containers;
+using VGAudio.Containers.Bxstm;
 using VGAudio.Formats;
 using Xunit;
 
@@ -15,6 +16,30 @@ namespace VGAudio.Tests.Containers
             GcAdpcmFormat audio = GenerateAudio.GenerateAdpcmSineWave(BuildParseTestOptions.Samples, numChannels, BuildParseTestOptions.SampleRate);
 
             BuildParseTests.BuildParseCompareAudio(audio, new BfstmWriter(), new BfstmReader());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(8)]
+        public void BfstmBuildAndParseEqualPcm16(int numChannels)
+        {
+            Pcm16Format audio = GenerateAudio.GeneratePcmSineWave(BuildParseTestOptions.Samples, numChannels, BuildParseTestOptions.SampleRate);
+            var writer = new BfstmWriter { Configuration = { Codec = BxstmCodec.Pcm16Bit } };
+
+            BuildParseTests.BuildParseCompareAudio(audio, writer, new BfstmReader());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(8)]
+        public void BfstmBuildAndParseEqualPcm8(int numChannels)
+        {
+            Pcm8SignedFormat audio = GenerateAudio.GeneratePcm8SignedSineWave(BuildParseTestOptions.Samples, numChannels, BuildParseTestOptions.SampleRate);
+            var writer = new BfstmWriter { Configuration = { Codec = BxstmCodec.Pcm8Bit } };
+
+            BuildParseTests.BuildParseCompareAudio(audio, writer, new BfstmReader());
         }
     }
 }
