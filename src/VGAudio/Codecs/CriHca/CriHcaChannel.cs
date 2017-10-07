86da8af97c7c998b9beb9543a34ade4637c25ad9
Alex Barney <thealexbarney@gmail.com>
2017-10-07 13:50:25 -0500

    Find optimal delta encoding size

--- src/VGAudio/Codecs/CriHca/CriHcaChannel.cs
+++ src/VGAudio/Codecs/CriHca/CriHcaChannel.cs
@@ -6,9 +6,13 @@ namespace VGAudio.Codecs.CriHca
     public class CriHcaChannel
     {
         public ChannelType Type { get; set; }
+        public int CodedScaleFactorCount { get; set; }
         public double[][] PcmFloat { get; } = Helpers.CreateJaggedArray<double[][]>(8, 128);
         public double[][] Spectra { get; } = Helpers.CreateJaggedArray<double[][]>(8, 128);
+        public double[][] ScaledSpectra { get; } = Helpers.CreateJaggedArray<double[][]>(8, 128);
         public Mdct Mdct { get; } = new Mdct(7, CriHcaTables.MdctWindow, Math.Sqrt(2.0 / 128));
         public int[] ScaleFactors { get; } = new int[128];
+        public int ScaleFactorBits { get; set; }
+        public int ScaleFactorDeltaBits { get; set; }
     }
 }
--- src/VGAudio/Codecs/CriHca/CriHcaEncoder.cs
+++ src/VGAudio/Codecs/CriHca/CriHcaEncoder.cs
@@ -53,7 +53,10 @@ namespace VGAudio.Codecs.CriHca
             {
                 Channels[i] = new CriHcaChannel
                 {
-                    Type = channelTypes[i]
+                    Type = channelTypes[i],
+                    CodedScaleFactorCount = channelTypes[i] == ChannelType.StereoSecondary
+                        ? Hca.BaseBandCount + Hca.StereoBandCount
+                        : Hca.BaseBandCount
                 };
             }
         }
@@ -70,6 +73,77 @@ namespace VGAudio.Codecs.CriHca
             PcmToFloat(PcmBuffer, Channels);
             RunMdct(Channels);
             CalculateScaleFactors(Channels, Hca);
+            ScaleSpectra(Channels, Hca);
+            CalculateScaleFactorLength(Channels);
+        }
+
+        private static void CalculateScaleFactorLength(CriHcaChannel[] channels)
+        {
+            foreach (CriHcaChannel channel in channels)
+            {
+                CalculateOptimalDeltaLength(channel);
+                if (channel.Type == ChannelType.StereoSecondary) channel.ScaleFactorBits += 32;
+            }
+        }
+
+        private static void CalculateOptimalDeltaLength(CriHcaChannel channel)
+        {
+            var emptyChannel = true;
+            for (int i = 0; i < channel.CodedScaleFactorCount; i++)
+            {
+                if (i != 0)
+                {
+                    emptyChannel = false;
+                    break;
+                }
+            }
+
+            if (emptyChannel)
+            {
+                channel.ScaleFactorBits = 3;
+                channel.ScaleFactorDeltaBits = 0;
+            }
+
+            int minDeltaBits = 6;
+            int minLength = 3 + 6 * channel.CodedScaleFactorCount;
+
+            for (int deltaBits = 1; deltaBits < 6; deltaBits++)
+            {
+                int maxDelta = (1 << (deltaBits - 1)) - 1;
+                int length = 3 + 6;
+                for (int band = 1; band < channel.CodedScaleFactorCount; band++)
+                {
+                    int delta = channel.ScaleFactors[band] - channel.ScaleFactors[band - 1];
+                    length += Math.Abs(delta) > maxDelta ? deltaBits + 6 : deltaBits;
+                }
+                if (length < minLength)
+                {
+                    minLength = length;
+                    minDeltaBits = deltaBits;
+                }
+            }
+
+            channel.ScaleFactorBits = minLength;
+            channel.ScaleFactorDeltaBits = minDeltaBits;
+        }
+
+        private static void ScaleSpectra(CriHcaChannel[] channels, HcaInfo hca)
+        {
+            foreach (CriHcaChannel channel in channels)
+            {
+                var bands = channel.Type == ChannelType.StereoSecondary ? hca.BaseBandCount : hca.TotalBandCount;
+
+                for (int b = 0; b < bands; b++)
+                {
+                    var scaleFactor = channel.ScaleFactors[b];
+                    for (int sf = 0; sf < 8; sf++)
+                    {
+                        var coeff = channel.Spectra[sf][b];
+                        channel.ScaledSpectra[sf][b] = scaleFactor == 0 ? 0 :
+                            coeff / CriHcaTables.DequantizerScalingTable[scaleFactor];
+                    }
+                }
+            }
         }
 
         private static void CalculateScaleFactors(CriHcaChannel[] channels, HcaInfo hca)
