37643e8825b6c13636e0cf3b7b933ee0f3784a65
Alex Barney <thealexbarney@gmail.com>
2017-12-11 16:58:14 -0600

    Update certificate thumbprint

--- build/Lifetime.cs
+++ build/Lifetime.cs
@@ -17,7 +17,7 @@ namespace Build
             context.BuildTargetsFile = context.SourceDir.CombineWithFilePath("Directory.Build.targets");
 
             context.AppxPublisher = "2E186599-2EB7-4677-93A5-C222C2F74D01";
-            context.ReleaseCertThumbprint = "2043012AE523F7FA0F77A537387633BEB7A9F4DD";
+            context.ReleaseCertThumbprint = "A81DF5034B182A7235B71B5524CCC9EE822BFA98";
         }
     }
 }
