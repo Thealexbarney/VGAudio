be0bfd5f286ad6a882acedcf71a961bc298c307a
Alex Barney <thealexbarney@gmail.com>
2018-11-27 18:57:13 -0600

    Opus encoder

--- src/VGAudio/Codecs/Opus/OpusFormat.cs
+++ src/VGAudio/Codecs/Opus/OpusFormat.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Concentus.Enums;
 using Concentus.Structs;
 using VGAudio.Containers.Opus;
 using VGAudio.Formats;
@@ -44,7 +45,7 @@ namespace VGAudio.Codecs.Opus
             {
                 dec.Decode(Frames[i].Data, 0, Frames[i].Data.Length, pcmBuffer, 0, maxSampleCount);
 
-                short[][] deinterleaved = pcmBuffer.DeInterleave(2, 2);
+                short[][] deinterleaved = pcmBuffer.DeInterleave(1, 2);
 
                 for (int c = 0; c < ChannelCount; c++)
                 {
@@ -59,7 +60,49 @@ namespace VGAudio.Codecs.Opus
 
         public override OpusFormat EncodeFromPcm16(Pcm16Format pcm16)
         {
-            throw new NotImplementedException();
+            const int frameSize = 960;
+            var encoder = new OpusEncoder(pcm16.SampleRate, pcm16.ChannelCount, OpusApplication.OPUS_APPLICATION_AUDIO);
+
+            int inPos = 0;
+            int remaining = pcm16.SampleCount;
+
+            short[] pcmData = pcm16.Channels.Interleave(1);
+
+            // Encoded data shouldn't be larger than the input pcm
+            var buffer = new byte[frameSize * pcm16.ChannelCount];
+            var frames = new List<NxOpusFrame>();
+
+            short[] encodeInput = pcmData;
+
+            while (remaining > 0)
+            {
+                int encodeCount = Math.Min(frameSize, remaining);
+
+                if (remaining < frameSize * pcm16.ChannelCount)
+                {
+                    encodeInput = new short[frameSize * pcm16.ChannelCount];
+                    Array.Copy(pcmData, inPos, encodeInput, 0, encodeCount);
+                    inPos = 0;
+                }
+
+                int frameLength = encoder.Encode(encodeInput, inPos, frameSize, buffer, 0, buffer.Length);
+
+                var frame = new NxOpusFrame();
+                frame.Length = frameLength;
+                frame.Data = new byte[frameLength];
+                frame.FinalRange = encoder.FinalRange;
+
+                Array.Copy(buffer, frame.Data, frameLength);
+
+                frames.Add(frame);
+
+                remaining -= encodeCount;
+                inPos += encodeCount * pcm16.ChannelCount;
+            }
+
+            OpusFormat format = new OpusFormatBuilder(pcm16.ChannelCount, pcm16.SampleCount, frames).Build();
+
+            return format;
         }
 
         protected override OpusFormat GetChannelsInternal(int[] channelRange)
